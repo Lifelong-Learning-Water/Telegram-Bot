fe13f3d95af6926b41b7686d9c7cb662ebc8c317
Gamma-Water <yzk416@dingtalk.com>
2025-04-08 18:51:26 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -51,7 +51,7 @@ async def send_to_telegram(platform, formatted_data):
     sent_message = await bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=message, parse_mode='Markdown')
 
     # 等待一段时间以确保消息被转发
-    await asyncio.sleep(2)
+    await asyncio.sleep(10)
 
     # 获取群组中的最新消息
     updates = await bot.get_updates()
