525704b32edcfba32eaab2ade94615eb09aa2e26
Gamma-Water <yzk416@dingtalk.com>
2025-04-08 18:32:49 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -78,9 +78,6 @@ async def send_to_telegram(platform, formatted_data):
         await asyncio.sleep(4)  # 避免请求过快
 
 async def main():
-    tz = pytz.timezone('Asia/Shanghai')
-    current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M")
-    await bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=f"北京时间: {current_time}", parse_mode='Markdown')
 
     for platform in PLATFROMS:
         print(f"正在获取：{platform[0]}")
