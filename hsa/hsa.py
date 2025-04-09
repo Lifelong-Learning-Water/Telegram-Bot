05e24a3c7902ff6e8581f7e73d3956113cbacf97
Gamma-Water <yzk416@dingtalk.com>
2025-04-10 00:11:57 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -72,7 +72,7 @@ async def fetch_news_data(source=None, category=None):
 
 async def translate_text(text):
     """调用翻译 API 翻译文本"""
-    url = f"https://findmyip.net/api/translate.php/text={text}"
+    url = f"https://v.api.aa1.cn/api/api-fanyi-yd/index.php?msg={text}&type=2"
     translated_data = await fetch_data(url, {})
     if translated_data and 'translated_text' in translated_data:
         return translated_data['translated_text']
