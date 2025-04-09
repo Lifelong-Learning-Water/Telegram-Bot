e3e872d7fa6ea6292e3e4ec504545312aaf92391
Gamma-Water <yzk416@dingtalk.com>
2025-04-09 19:43:02 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -72,8 +72,13 @@ def format_data(data_list, url_key, is_news=False):
     for index, item in enumerate(data_list):
         title = item.get('title', '无标题')
         url = item.get(url_key, '#')
-        hot_info = f"_{item.get('hot', '无热度')}🔥_" if not is_news and item.get('hot') else ""        
-        desc = f"\n_{item.get('desc', '无摘要')}_ " if item.get('desc') else ""
+        hot_info = f"_{item.get('hot')}🔥_" if not is_news and item.get('hot') else ""
+        if is_news:
+            desc = f"\n{item.get('description')}"
+        elif item.get('desc'):
+            desc = f"\n{item.get('desc')}"
+        else:
+            desc = ''
 
         formatted_string = f"{index + 1}. [{title}]({url}){hot_info}{desc}".strip()
         formatted_data.append(formatted_string)
