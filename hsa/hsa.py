8d482c2fa108b9e6791f5f097886f20aacbb774a
Gamma-Water <yzk416@dingtalk.com>
2025-04-10 15:34:51 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -179,7 +179,7 @@ async def main():
             message_info = await send_to_telegram(media[0], formatted_news)
             all_message_info.append(message_info)  # 添加到总消息信息列表
         await asyncio.sleep(2)
-
+"""
     for category in CATEGORIES:
         print(f"正在获取：{category[0]}")
         articles = await fetch_news_data(category=category[1])
@@ -188,13 +188,13 @@ async def main():
             message_info = await send_to_telegram(category[0], formatted_news)
             all_message_info.append(message_info)  # 添加到总消息信息列表
         await asyncio.sleep(2)
-
+"""
     if all_message_info:
         jump_message = "点击链接查看榜单：\n"
         links = []
 
         for info in all_message_info:
-            link = f"<a href='https://t.me/热搜聚合/{info['id']}'>{escape_html(info['name'])}</a>"
+            link = f"<a href='https://t.me/@delta_water_hsa_bot/{info['id']}'>{escape_html(info['name'])}</a>"
             links.append(link)
 
         jump_message += "\n".join(links)
