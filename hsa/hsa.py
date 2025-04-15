ceb76dffc6c8161543f7a8cafdebb58ace35d4cb
Gamma-Water <yzk416@dingtalk.com>
2025-04-15 20:53:32 +0800

    更新 hsa.py

--- hsa/hsa.py
+++ hsa/hsa.py
@@ -166,7 +166,7 @@ async def main():
     await asyncio.sleep(2)
 
     all_message_info = []  # 用于记录所有热搜榜单的消息 ID 和名称
-
+    """
     for category in CATEGORIES:
         print(f"正在获取：{category[0]}")
         articles = await fetch_news_data(category=category[1])
@@ -175,7 +175,7 @@ async def main():
             message_info = await send_to_telegram(category[0], formatted_news)
             all_message_info.append(message_info)
         await asyncio.sleep(2)
-
+    """
     for media in FOREIGN_MEDIA:
         print(f"正在获取：{media[0]}")
         articles = await fetch_news_data(source=media[1])
